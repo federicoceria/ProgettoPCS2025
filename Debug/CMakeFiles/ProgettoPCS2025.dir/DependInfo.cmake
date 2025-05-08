67d497e46da664b2ecbc32389d63471c0570dac1
GabriCanno <s310170@studenti.polito.it>
2025-05-08 15:32:45 +0200

    creazione CMakeLists

---
+++ Debug/CMakeFiles/ProgettoPCS2025.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/appuser/Data/ProgettoPCS2025/ExportParaview/UCDUtilities.cpp" "CMakeFiles/ProgettoPCS2025.dir/ExportParaview/UCDUtilities.cpp.o" "gcc" "CMakeFiles/ProgettoPCS2025.dir/ExportParaview/UCDUtilities.cpp.o.d"
+  "/home/appuser/Data/ProgettoPCS2025/main.cpp" "CMakeFiles/ProgettoPCS2025.dir/main.cpp.o" "gcc" "CMakeFiles/ProgettoPCS2025.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
