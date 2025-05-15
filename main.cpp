8876812932d3528747d42eaede29d1a6afcad8df
federicoceria <s308599@studenti.polito.it>
2025-05-15 22:32:37 +0200

    testo GenerateGeodeticPolyhedronType1

--- main.cpp
+++ main.cpp
@@ -1,28 +1,44 @@
 #include "Utils.hpp"
 #include <iostream>
+#include <sstream>
+#include "PolyhedralMesh.hpp"
 
 using namespace std;
 using namespace PolyhedralLibrary;
 
 int main()
 {
-	string path = "home/appuser/Data/progettoPCS2025/Platonic_solids";
-	// string File_0D_exp = ...
-	// string File_1D_path = ...
-	
-	PolyhedralMesh mesh;
-	
-	cout << "A posto" << endl;
-/*	if (!ImportVector(path, mesh)
-		return 1;
-	
-	if (!ExpPoints(mesh, File_0D_exp)
-		return 1;
-	
-	if (!ExpSegments(mesh, File_1D_exp)
-		return 1;
-	
-	
-*/	
-	return 0;
+    string path = "/home/appuser/Data/progettoPCS2025/Platonic_solids/Icosaedro";
+    
+    // Mesh di partenza e quella generata
+    PolyhedralMesh icosahedron;
+    PolyhedralMesh geodetic;
+
+    // Numero di segmenti da usare per la suddivisione
+    int num_segments = 3;
+
+    // Importa la mesh dell'icosaedro
+    if (!ImportMesh(path, icosahedron)) {
+        cerr << "Error importing the Icosahedron." << endl;
+        return 1;
+    }
+
+    cout << "Icosahedron successfully imported." << endl;
+    cout << "Number of vertices: " << icosahedron.NumCell0Ds << endl;
+    cout << "Number of edges: " << icosahedron.NumCell1Ds << endl;
+    cout << "Number of faces: " << icosahedron.NumCell2Ds << endl;
+
+    // Genera il solido geodetico
+    if (!GenerateGeodeticPolyhedronType1(icosahedron, geodetic, num_segments)) {
+        cerr << "Error generating the geodetic solid." << endl;
+        return 1;
+    }
+
+    cout << "Geodetic solid successfully generated with num_segments = " << num_segments << endl;
+    cout << "Number of vertices: " << geodetic.NumCell0Ds << endl;
+    cout << "Number of edges: " << geodetic.NumCell1Ds << endl;
+    cout << "Number of faces: " << geodetic.NumCell2Ds << endl;
+
+    return 0;
 }
+
